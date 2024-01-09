b0b2573b590bf18cd332b252296c1f128a4a82af
Takiplastic <dtlightning1358@gmail.com>
2024-01-09 18:51:23 +0900

    1/9Daiki_Takaku
    石田さんが作成した予定作成機能を統合
    カレンダー画面で画面下のプラスボタンを押すことで予定作成画面へ移行
    セーブファイルの場所を相対参照に変更(誰のパソコンでも動くはず)

---
+++ Mycalender/Assets/Script/decidebutton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class decidebutton : MonoBehaviour
+{
+    DateTime starttime=setstartday.starttime;
+    DateTime finish= setstartday.finish;
+    public class Data
+    {
+     public int StartY;
+     public int StartM;
+     public int StartD;
+     public int FinishY;
+     public int FinishM;
+     public int FinishD;
+    }
+    // Start is called before the first frame update
+    public void OnClickdecideButton()
+    {
+        Data schedule = new Data();
+        schedule.StartY = int.Parse(starttime.ToString("yyyy"));
+        schedule.StartM = int.Parse(starttime.ToString("MM"));
+        schedule.StartD = int.Parse(starttime.ToString("dd"));
+        schedule.FinishY = int.Parse(finish.ToString("yyyy"));
+        schedule.FinishM = int.Parse(finish.ToString("MM"));
+        schedule.FinishD = int.Parse(finish.ToString("dd"));
+        string jsonschedule = JsonUtility.ToJson(schedule);
+        Debug.Log(jsonschedule);
+        string path = Application.dataPath + "/savedata.json"; /* 既存のJSONファイルのパス */
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(jsonschedule);
+        writer.Close();
+
+
+        SceneManager.LoadScene("Calender");
+    }
+}
